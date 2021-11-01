8ea4274f383078cf95703025617e4da92fd3324c
Igor Sousa Do Nascimento <igor.nascimento@totvs.com.br>
2021-11-01 00:13:53 -0300

    Versão inicial do app apenas com Get dos usuários.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomePage } from './home/home.page';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
